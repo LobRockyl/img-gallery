3794c5d211a0c00d9e1dd7ea5748ee76f43c8e3b
LobRockyl <lobrockyl@gmail.com>
2022-02-12 16:39:52 +0530

    staging heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI, {
+  const conn = await mongoose.connect("mongodb+srv://purnashis:purnashis@cluster0.lhecz.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
